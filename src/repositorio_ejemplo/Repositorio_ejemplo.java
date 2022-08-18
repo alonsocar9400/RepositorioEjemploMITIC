2e633c34249503dcbc5c1e1285099bb70baf1fd9
Hector2022 <alonsocar9400@gmail.com>
2022-08-17 21:25:18 -0500

    version inicial

---
+++ src/repositorio_ejemplo/Repositorio_ejemplo.java
@@ -0,0 +1,14 @@
+
+package repositorio_ejemplo;
+
+
+public class Repositorio_ejemplo {
+
+   
+    public static void main(String[] args) {
+        
+        System.out.println("Probando repositorio");
+    
+    }
+    
+}
